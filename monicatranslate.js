8efc6dda9cb56b211eff12d60f0dbc7f477fa52d
jonas <jonasjso@protonmail.com>
2023-03-10 12:29:12 +0100

    Quality of life improvements
    * Autofill text from focused input
    * Autocopy text to clipboard
    * Change from shift-T to ctrl-shift-T to fix input bug

--- monicatranslate.js
+++ monicatranslate.js
@@ -1,14 +1,19 @@
 
-window.addEventListener("keydown", ({key, shiftKey})=> {
-    console.info(shiftKey, key)
-    if (key.toLocaleLowerCase() == "t" && !!shiftKey){
+const inputs = document.querySelectorAll("input")
+inputs.forEach(input => {
+    input.addEventListener("keydown", ({ key, shiftKey, ctrlKey, metaKey, target })=> {
+        console.info(`monicattranslate: keydown: shiftKey:${shiftKey}, ctrlKey:${ctrlKey}, key:${key}`)
 
-        let key = window.getSelection()
-        let currentValue = window.localStorage.getItem(key) ?? key
-
-        let newValue = window.prompt("Translate this", currentValue)
-        if (newValue != currentValue) {
-            window.localStorage.setItem(key, newValue)
+        if (key.toLocaleLowerCase() == "t" && ctrlKey && shiftKey) {
+            
+            let key = target.value
+            let currentValue = window.localStorage.getItem(key) ?? key
+    
+            let newValue = window.prompt(`Translate: "${currentValue}"`, currentValue)
+            if (newValue) {
+                navigator.clipboard.writeText(newValue);
+                window.localStorage.setItem(key, newValue)
+            }
         }
-    }
-})
+    })
+});
