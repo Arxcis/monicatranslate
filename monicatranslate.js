2e81fdd22545a10c67075d6a93310647454563bb
jonas <jonasjso@protonmail.com>
2023-03-11 00:40:21 +0100

    change to ctrl+u

--- monicatranslate.js
+++ monicatranslate.js
@@ -1,10 +1,11 @@
 
 const inputs = document.querySelectorAll("input")
+
 inputs.forEach(input => {
-    input.addEventListener("keydown", ({ key, shiftKey, ctrlKey, metaKey, target })=> {
+    input.addEventListener("keydown", ({ key, shiftKey, ctrlKey, target })=> {
         console.info(`monicattranslate: keydown: shiftKey:${shiftKey}, ctrlKey:${ctrlKey}, key:${key}`)
 
-        if (key.toLocaleLowerCase() == "t" && ctrlKey && shiftKey) {
+        if (key.toLocaleLowerCase() == "u" && ctrlKey) {
             
             let key = target.value
             let currentValue = window.localStorage.getItem(key) ?? key
